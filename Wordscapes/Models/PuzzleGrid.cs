07839eb1dee97b7bc58590c332e17d29975ea8ca
RotariuPaul <paulrotariu07@gmail.com>
2026-06-07 16:48:28 +0300

    6/7

--- Wordscapes/Models/PuzzleGrid.cs
+++ Wordscapes/Models/PuzzleGrid.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Wordscapes.Models;
 
-namespace Wordscapes.GameModes
+namespace Wordscapes.Models
 {
     public class PuzzleGrid
     {
